5611bd44dd519406760950f5709dd73b169381c8
Danial Mazarei <71553400+dmazarei@users.noreply.github.com>
2024-05-07 09:58:05 +0330

    add

---
+++ R/remission.R
@@ -0,0 +1,22 @@
+#' Remission data
+#'
+#' It is related to remission time in months of 128 cancer patients.
+#'
+#'
+#' @name remission
+#'
+#' @format
+#' A data.frame with 128 observations of remission time in months of cancer patients.
+#'
+#' @source
+#' Lee, E.T. and Wang, J. (2003), Statistical Methods for Survival Data Analysis.
+#'  Vol. 476, John Wiley & Sons,Hoboken, NJ, USA.
+#'
+#' Rao, G. S., Norouzirad, M., and Mazarei . D. (2023). Neutrosophic
+#'    Generalized Exponential Distribution with Application.
+#'    \emph{Neutrosophic Sets and Systems}, 55, 471-485.
+#'
+#' @examples
+#' data("remission")
+#' remission
+NULL
