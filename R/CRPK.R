70635ad33deb7cf0ed349dad9ac9a9a51459ecc1
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 14:58:54 +0100

    Add dataset "Corona Pakistan"

---
+++ R/CRPK.R
@@ -0,0 +1,18 @@
+#' Coronavirus Reproduction Rate Data for Pakistan
+#'
+#' Dataset containing the coronavirus reproduction rate in Pakistan from October 2020 to December 2020.
+#'
+#' @name CRPK
+#'
+#' @format
+#' A data frame with 79 observations of coronavirus reproduction rate in Pakistan from October 2020 to December 2020.
+#'
+#' @source
+#' Sherwani, R. A. K., Shakeel, H., Saleem, M., Awan, W. B., Aslam, M.,
+#' Farooq, M. (2021). A new neutrosophic sign test: An application to COVID-19 data,
+#' PLOS ONE, 16(8): e0255671.
+#'
+#' @examples
+#' data("CRPK")
+#' head(CRPK)
+NULL
