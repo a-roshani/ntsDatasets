48980c772e884626c3146c6f8d8fd6d0fc68506a
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 11:41:33 +0100

    Add dataset "batteries"

---
+++ R/batteries.R
@@ -0,0 +1,21 @@
+#' Lifetime of batteries data
+#'
+#' It is related to the lifetime in 100hours of batteries
+#'
+#' @name batteries
+#'
+#' @format
+#' A data.frame with 23 batteries represent their lifetime.
+#'
+#' @source
+#' Aslam, M. (2021). A new goodness of fit test in the presence of uncertain
+#' parameters. \emph{Complex and Intelligent Systems}, 7(1), 359–365.
+#'
+#' Hassan, M. K. and Aslam, M. (2024). Birnbaum Saunders distribution for
+#' imprecise data: statistical properties, estimation methods, and real life
+#' applications, \emph{scientific reports}, 14: 6955.
+#'
+#' @examples
+#' data("batteries")
+#' batteries
+NULL
