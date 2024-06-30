4748ad5e9c1de4588448e89ebd29cf0d39accb6e
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 15:45:30 +0100

    Add dataset "AGSTimes"

---
+++ R/AGSTimes.R
@@ -0,0 +1,19 @@
+#' The Analgesic Datasets
+#'
+#' Dataset containing times to relief for 20 patients receiving analgesic.
+#'
+#' @name AGSTimes
+#'
+#' @format
+#' A data frame with the relief times (in minutes) of 20 patients receiving analgesic.
+#'
+#' @source
+#' Jamal, F., Shafiq, S., Aslam, M., Khan, S., Hussain, Z., Abbas, Q. (2024).
+#' Modeling COVID-19 data with a novel neutrosophic Burr-III distribution,
+#' \emph{Scientific Reports}, 14:10810.
+#'
+#' @examples
+#' data("AGSTimes")
+#' AGSTimes
+NULL
+
