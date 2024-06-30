ad47e54461c13af694d4f9790c3f0f0ea1f74857
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 16:33:30 +0100

    update the dataset 'ICUcovidPK'

--- R/ICUcovidPK.R
+++ R/ICUcovidPK.R
@@ -10,15 +10,17 @@
 #'   - age_55_and_above: Lower and upper bounds of ICU occupancy for patients aged 55 and above, formatted as "lower-upper".
 #'   - age_between_35_and_55: Lower and upper bounds of ICU occupancy for patients aged between 35 and 55, formatted as "lower-upper".
 #'   - age_below_35: Lower and upper bounds of ICU occupancy for patients aged below 35, formatted as "lower-upper".
+#'   - male: Lower and upper bounds of ICU occupancy for male patients, formatted as "lower-upper".
+#'   - female: Lower and upper bounds of ICU occupancy for female patients, formatted as "lower-upper".
 #'
 #' @source
 #' Sherwani, R. A. K., Shakeel, H., Awan, W. B., Faheem, M., Aslam, M. (2021).
 #' Analysis of COVID-19 data using neutrosophic Kruskal Wallis H test, \emph{BMC
 #' Medical Research Methodology}, 21:215.
 #'
-#'
 #' @examples
 #' data("ICUcovidPK")
 #' ICUcovidPK
 NULL
+
 
