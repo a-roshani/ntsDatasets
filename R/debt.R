a1095b2ba3399b42cdccaeec6d674699dead92e2
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 13:58:26 +0100

    Add dataset "debt"

---
+++ R/debt.R
@@ -0,0 +1,18 @@
+#' Public debt data
+#'
+#' It is related to the public debt (in million dollar) in Egypt covering the period from 2000 to 2022.
+#'
+#' @name debt
+#'
+#' @format
+#' A data.frame with 23 observations of public debt in Egypt in 200-2022.
+#'
+#' @source
+#' Essa, N. I., Zhar, H. M., Abu El-Magd, N. A. T. (2023). Neutrosophic
+#' Generalized Pareto Distribution, \emph{Mathematics and Statistics}, 11(5),
+#' 827-833.
+#'
+#' @examples
+#' data("debt")
+#' debt
+NULL
