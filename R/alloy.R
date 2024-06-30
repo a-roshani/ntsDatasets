cb6018f4a01f9dd25b9d1c3bcbdcc096c4b2869f
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 10:48:38 +0100

    Add dataset "alloy"

---
+++ R/alloy.R
@@ -0,0 +1,22 @@
+#' Alloy melting data
+#'
+#' It is related to alloy melting points.
+#'
+#' @name alloy
+#'
+#' @format
+#' A data.frame with 18 observations of alloy melting points.
+#'
+#' @source
+#' Rao, G. S.  (2003). Neutrosophic Log-Logistic Distribution Model in Complex
+#' Alloy Metal Melting Point Applications, \emph{International Journal of
+#' Computational Intelligence Systems}, 16: 48.
+#'
+#' Hassan, M. K. and Aslam, M. (2024). Birnbaum Saunders distribution for
+#' imprecise data: statistical properties, estimation methods, and real life
+#' applications, \emph{scientific reports}, 14: 6955.
+#'
+#' @examples
+#' data("alloy")
+#' alloy
+NULL
