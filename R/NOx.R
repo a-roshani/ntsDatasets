ed59f2648a69c7bec625a1c3737d749249f2bac7
mnrzrad <mina.norouzirad@gmail.com>
2024-06-30 14:22:43 +0100

    Add dataset "NOx"

---
+++ R/NOx.R
@@ -0,0 +1,19 @@
+#' Nitrogen Oxides Emissions Data
+#'
+#' This dataset includes yearly nitrogen oxides emissions for Denmark for the period 1990 to 2018.
+#'
+#' @name NOx
+#'
+#' @format
+#' A data frame with 29 observations of nitrogen oxides emissions for Denmark for the period 1990-2018.
+#'
+#' @source
+#' Khan, Z., Amin, A., Khan, S. A., Gulistan, M. (2021). Statistical Development
+#' of the Neutrosophic Lognormal Model with Application to Environmental Data,
+#' \emph{Neutrosophic Sets and Systems}, 47, 1-11.
+#'
+#' @examples
+#' data("NOx")
+#' NOx
+NULL
+
