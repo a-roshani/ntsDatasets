c49714761da222b16899c4b454aee41d19bb7ee2
mnrzrad <mina.norouzirad@gmail.com>
2024-07-09 15:50:17 +0100

    Add Failure dataset

---
+++ R/failure.R
@@ -0,0 +1,18 @@
+#' Failur time data
+#'
+#' It is related to failure times of the cooling system used in an airplane.
+#'
+#'
+#' @name remission
+#'
+#' @format
+#' A data.frame with 30 observations of failure time data on the air conditioning system
+#'
+#' @source
+#' Linhart, H. and Zucchini, W. (1986). \emph{Model Selection}, John Wiley & Sons, New York.
+#'
+#'
+#' @examples
+#' data("failure")
+#' failure
+NULL
