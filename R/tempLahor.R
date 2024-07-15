e70c53a35c281a421a7b8d6751f92de10e7302c1
mnrzrad <mina.norouzirad@gmail.com>
2024-07-15 16:39:58 +0100

    Add dataset "tempLahor"

---
+++ R/tempLahor.R
@@ -0,0 +1,18 @@
+#' Monthly Low and High Temperatures in Lahore, Pakistan (2016–2020)
+#'
+#' This dataset contains the monthly low and high temperatures recorded in Lahore, Pakistan, over the years 2016 to 2020.
+#'
+#' @name tempLahor
+#'
+#' @format
+#' A data frame with 60 rows representing months, and columns for low and high temperatures in Lahore, Pakistan, from 2016 to 2020.
+#'
+#' @source
+#' Ahsan-ul-Haq, M., Zafar, J., Aslam, M., Tariq, S. (2024).
+#' Neutrosophic Topp-Leone Distribution for Interval-Valued Data Analysis,
+#' \emph{Journal of Statistical Theory and Applications}, 23, 164–173.
+#'
+#' @examples
+#' data("tempLahor")
+#' tempLahor
+NULL
