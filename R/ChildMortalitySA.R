21cca6405938c83fcc1a67eff92e9c57f04edbde
mnrzrad <mina.norouzirad@gmail.com>
2024-07-15 18:18:52 +0100

    Add dataset "ChildMortalitySA"

---
+++ R/ChildMortalitySA.R
@@ -0,0 +1,16 @@
+#' Childhood Mortality Rates in Saudi Arabia (1995-2020)
+#'
+#' Dataset containing childhood mortality rates (under age 5) per 1000 live births for Saudi Arabia from 1995 to 2020. Data sourced from the WHO's global health indicators database.
+#'
+#' @name ChildMortalitySA
+#'
+#' @format
+#' A data frame with observations of childhood mortality rates per 1000 live births.
+#'
+#' @source
+#' World Health Organization (WHO), Global Health Observatory (GHO) data repository.
+#'
+#' @examples
+#' data("ChildMortalitySA")
+#' ChildMortalitySA
+NULL
