3f9e277d458549fbb22e6a26ff0b5b6b607286a1
mnrzrad <mina.norouzirad@gmail.com>
2024-07-15 17:55:38 +0100

    Add datasets "dioxins"

---
+++ R/dioxins.R
@@ -0,0 +1,18 @@
+#' Average Daily Ingestion of Dioxins Data
+#'
+#' The dataset contains estimates of dioxins absorption from the average diet annually for the years 1998 to 2015, as reported in the 2017 annual environmental statistics by the Ministry of Environment, Japan.
+#'
+#' @name dioxins
+#'
+#' @format
+#' A data frame with 18 observations. Each observation represents the estimated daily ingestion of dioxins (in pg TEQ/day).
+#'
+#' @source
+#' Khan, Z., Almazah, M. M. A., Hamood Odhah, O., Alshanbari, H. M. (2022).
+#' Generalized Pareto Model: Properties and Applications in Neutrosophic Data
+#' Modeling, \emph{Mathematical Problems in Engineering}, 2022, 3686968.
+#'
+#' @examples
+#' data("dioxins")
+#' dioxins
+NULL
