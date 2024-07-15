7dc6e5a1a1dd1287168956388ccc4514429cdea8
mnrzrad <mina.norouzirad@gmail.com>
2024-07-15 16:07:38 +0100

    Add dataset "PopVillUSA"

---
+++ R/PopVillUSA.R
@@ -0,0 +1,22 @@
+#' Population Villages USA data
+#'
+#' It is related to population compactness of a few villages in rural USA.
+#'
+#' @name PopVillUSA
+#'
+#' @format
+#' A data frame with 17 populations of Villages in the USA. Each row represents a village.
+#'
+#' @source
+#' Albassam, M., Khan, N., Aslam, M., (2020) The W/S Test for Data Having
+#' Neutrosophic Numbers: An Application to USA Village Population,
+#' \emph{Complexity}, 2020, 3690879.
+#'
+#' Rao, G. S., Norouzirad, M., and Mazarei, D. (2023). Neutrosophic
+#' Generalized Exponential Distribution with Application.
+#' \emph{Neutrosophic Sets and Systems}, 55, 471-485.
+#'
+#' @examples
+#' data("PopVillUSA")
+#' PopVillUSA
+NULL
